bf5844c00c346123bef162a8985cfa820feeea63
Steve Schmechel <shmakes@gmail.com>
2025-02-18 07:34:39 -0600

    Fix the URL on the temporary "msg" endpoint so it confirms DB access again.

--- routes/msg.js
+++ routes/msg.js
@@ -1,13 +1,14 @@
 import { Message } from '../models/message.js';
 
 const dbUrl = process.env.DB_URL;
+const dbName = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 
 async function testdb() {
     try {
         const auth = Buffer.from(`${dbUser}:${dbPass}`).toString('base64');
-        const response = await fetch(dbUrl, {
+        const response = await fetch(`${dbUrl}/${dbName}`, {
             headers: {
                 'Authorization': `Basic ${auth}`,
                 'Accept': 'application/json'
